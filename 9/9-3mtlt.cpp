4653c0e0e0bbe6863ed86affbe82295ec42638f8
15797736638 <51181925+15797736638@users.noreply.github.com>
2021-04-20 20:27:38 +0800

    Update 9-3mtlt.cpp

--- 9/9-3mtlt.cpp
+++ 9/9-3mtlt.cpp
@@ -22,35 +22,41 @@ int setnonblocking( int fd )
     fcntl( fd, F_SETFL, new_option );
     return old_option;
 }
-
+//将文件描述符fd上的EPOLLIN注册到epollfd指示的epoll内核事件表中，参数enable_et指定是否对fd启用ET模式
 void addfd( int epollfd, int fd, bool enable_et )
 {
     epoll_event event;
     event.data.fd = fd;
-    event.events = EPOLLIN;
+    event.events = EPOLLIN;//EPOLL的可读事件是EPOLLIN
     if( enable_et )
     {
         event.events |= EPOLLET;
     }
     epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &event );
+    //EPOLL_CTL_ADD,往事件表注册fd上的事件
+    //EPOLL_CTL_MODE，修改fd上的注册事件
+    //EPOLL_CTL_DEL，删除fd上的注册事件
     setnonblocking( fd );
 }
-
+//LT模式的工作流程
 void lt( epoll_event* events, int number, int epollfd, int listenfd )
-{
+{// 就绪的事件写在events里，number是就绪事件的数量，epollfd指定内核时间表，
     char buf[ BUFFER_SIZE ];
     for ( int i = 0; i < number; i++ )
     {
         int sockfd = events[i].data.fd;
         if ( sockfd == listenfd )
-        {
+        {//监听socket有新的连接请求
             struct sockaddr_in client_address;
             socklen_t client_addrlength = sizeof( client_address );
             int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
-            addfd( epollfd, connfd, false );
+            addfd( epollfd, connfd, false );//往内核事件表epollfd中注册连接socket connfd的可读事件
         }
         else if ( events[i].events & EPOLLIN )
+        //只要socket读缓存中还有未读出的数据，这段代码就被触发：：：LT工作模式，电平触发，下一次调用epoll_wait
+        //时，还会再次向应用程序告知此事件            
         {
+            //如果发生的事件不是监听socket，而是连接socket，将数据写入缓冲区
             printf( "event trigger once\n" );
             memset( buf, '\0', BUFFER_SIZE );
             int ret = recv( sockfd, buf, BUFFER_SIZE-1, 0 );
@@ -79,10 +85,12 @@ void et( epoll_event* events, int number, int epollfd, int listenfd )
             struct sockaddr_in client_address;
             socklen_t client_addrlength = sizeof( client_address );
             int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
-            addfd( epollfd, connfd, true );
+            addfd( epollfd, connfd, true );//往epollfd指定的内核事件表中注册连接socket:connfd的可读事件
+            // true：ET工作模式，只提醒一次
         }
         else if ( events[i].events & EPOLLIN )
         {
+            /*这段代码不会被重复触发， 所以我们循环读取数据， 以确保把socket读缓存中的所有数据读出*/
             printf( "event trigger once\n" );
             while( 1 )
             {
@@ -142,13 +150,15 @@ int main( int argc, char* argv[] )
     assert( ret != -1 );
 
     epoll_event events[ MAX_EVENT_NUMBER ];
-    int epollfd = epoll_create( 5 );
+    int epollfd = epoll_create( 5 ); //创建标识事件表的文件描述符
     assert( epollfd != -1 );
-    addfd( epollfd, listenfd, true );
+    addfd( epollfd, listenfd, true );//true启用ET模式
 
     while( 1 )
     {
         int ret = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
+        //在一段超时时间内等待一组文件描述符上的事件，成功时返回就绪的文件描述符的个数
+        //如果检测到事件，就将所有就绪的事件从epollfd中复制到它的第二个参数events指向的数组
         if ( ret < 0 )
         {
             printf( "epoll failure\n" );
