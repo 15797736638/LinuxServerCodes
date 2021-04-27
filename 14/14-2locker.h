f3e36ab701a67c4383d1dabd286c6fff533db477
15797736638 <51181925+15797736638@users.noreply.github.com>
2021-04-27 16:12:56 +0800

    Update 14-2locker.h

--- 14/14-2locker.h
+++ 14/14-2locker.h
@@ -4,26 +4,27 @@
 #include <exception>
 #include <pthread.h>
 #include <semaphore.h>
-
+//封装信号量的类
 class sem
 {
 public:
+    //创建并初始化信号量
     sem()
     {
         if( sem_init( &m_sem, 0, 0 ) != 0 )
-        {
+        {//构造函数没有返回值，可以通过抛出异常来报告错误
             throw std::exception();
         }
     }
-    ~sem()
+    ~sem()//销毁信号量
     {
         sem_destroy( &m_sem );
     }
-    bool wait()
+    bool wait()//等待信号量
     {
         return sem_wait( &m_sem ) == 0;
     }
-    bool post()
+    bool post()//增加信号量
     {
         return sem_post( &m_sem ) == 0;
     }
@@ -32,7 +33,7 @@ private:
     sem_t m_sem;
 };
 
-class locker
+class locker//封装互斥锁的类
 {
 public:
     locker()
@@ -59,17 +60,17 @@ private:
     pthread_mutex_t m_mutex;
 };
 
-class cond
+class cond//封装条件变量的类
 {
 public:
-    cond()
+    cond()//创建并初始化条件变量
     {
         if( pthread_mutex_init( &m_mutex, NULL ) != 0 )
         {
             throw std::exception();
         }
         if ( pthread_cond_init( &m_cond, NULL ) != 0 )
-        {
+        {//构造函数出现问题，就应该立即释放已经成功分配了的资源
             pthread_mutex_destroy( &m_mutex );
             throw std::exception();
         }
